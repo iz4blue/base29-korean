5fbb191c03a0a7c7b586993c85c5f272f108896e
김정환 <iz4blue@users.noreply.github.com>
2019-05-15 15:33:22 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@
 from setuptools import setup
 
 setup(
-    name='base30-korean',
+    name='base29-korean',
     version='0.0.2',
-    py_modules=['base30_korean'],
+    py_modules=['base29_korean'],
     author='Joseph Kim',
     author_email='iz4blue@gmail.com',
     description='crockford base32를 한국어 실정에 맞게 변형한 base30',
-    url='https://github.com/iz4blue/base30-korean',
+    url='https://github.com/iz4blue/base29-korean',
     license='BSD',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
